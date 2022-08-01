a0312c94927d5149723613bfd2bfa21027bd8970
Shvartseril <Shvartseril@yandex.ru>
2022-08-02 01:54:32 +0300

    Added a snake's head and the ability to move using the keys

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import pygame
 import random
-
 from field import Field
+import time
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 3
+
+# Создаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+REMOTE_COLOR = (0, 204, 205)
+
+
+WIDTH = 400
+HEIGHT = 600
+FPS = 30
+
+x = 300
+y = 300
+
+x_change = 0
+y_change = 0
+
 # Создаем игру и окно
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,10 +33,35 @@ field = Field(screen)
 running = True
 while running:
     # Держим цикл на правильной скорости
-    clock.tick(FPS)
-    field.draw_field()
     # Ввод процесса (события)
     for event in pygame.event.get():
-        # check for closing window
+
+        # Перемещаем голову змейки по клавишам: вверх, вниз, направо, налево
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -10
+                y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x_change = 10
+                y_change = 0
+            elif event.key == pygame.K_DOWN:
+                x_change = 0
+                y_change = 10
+            elif event.key == pygame.K_UP:
+                x_change = 0
+                y_change = -10
+
+    x += x_change
+    y += y_change
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, RED, [x, y, 10, 10])
+
+    pygame.display.update()
+
+    clock.tick(30)
+    time.sleep(0.1)
+
+pygame.quit()
+quit()
